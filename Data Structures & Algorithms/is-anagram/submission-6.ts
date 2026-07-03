ce82c3fdc83514bae22e9e0bfe3b777b2d3a1789
renan martins <r.ribeiromartins@bowdoin.edu>
2026-07-03 01:05:32 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length != t.length) return false
+
+        var arr = []
+        for (let sCh = 0; sCh < s.length; sCh++) {
+            //if (t.indexOf(s[sCh]) == -1) return false
+            const code = s.charCodeAt(sCh)
+            arr[code] = (arr[code] || 0) + 1
+        }
+
+        for (let tCh = 0; tCh < t.length; tCh++) {
+            const code = t.charCodeAt(tCh)
+            if (!arr[code]) { return false }
+            arr[code] = (arr[code] || 0) - 1
+        }
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) { return false }
+        }
+
+        return true
+    }
+}
