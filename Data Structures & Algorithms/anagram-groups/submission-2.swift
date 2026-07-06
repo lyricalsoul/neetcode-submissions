647decbe020832ccf17854da0ac813cc462f440c
renan martins <r.ribeiromartins@bowdoin.edu>
2026-07-06 19:55:42 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        var anagrams = [Int:[String]]()
+
+        for var str in strs {
+            var stStr = str.sorted().hashValue
+            if anagrams[stStr] != nil {
+                anagrams[stStr]!.append(str)
+            } else {
+                anagrams[stStr] = [str]
+                continue
+            }
+        }
+
+        return anagrams.map { $0.value }
+    }
+}
