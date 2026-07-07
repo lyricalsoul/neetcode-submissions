437677bd8fb4d4000cac47e9a1273efc6d0fae63
renan martins <r.ribeiromartins@bowdoin.edu>
2026-07-06 20:51:55 -0400

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func longestConsecutive(_ nums: [Int]) -> Int {
+        guard nums.count > 1 else { return nums.count }
+
+        let sorted = Array(Set(nums)).sorted()
+
+        var curr = 1
+        var largest = 0
+
+        for i in 1..<sorted.count {
+            if sorted[i - 1] + 1 == sorted[i] {
+                curr += 1
+            } else {
+                largest = max(curr, largest)
+                curr = 1
+            }
+        }
+
+        return max(curr, largest)
+    }
+}
